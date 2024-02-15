a5353c34a6aebb1493e4dbc2f56c0a5c4627f548
Kar <royjason715@gmail.com>
2024-02-15 22:00:13 +0530

    Variable to let, const and var

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,123 @@
+/*
 let js = 'Amazing';
-if (js === 'Amazing') alert ('JavaScript Is Fun');
-
 console.log(10-5+20);
+
+console.log('Jonas');
+console.log(23);
+
+// Values and Variables {Naming Conventions}
+
+// CamelCase is mostly used
+let firstname = "Matilda";
+console.log(firstname)
+
+// We can use underscore also
+let jonas_matilda = "JM";
+
+// reserve keywords for JavaScript. $ or _ to be put first
+let $function = 27
+
+// Some of the fixed value variables
+let PI = 3.14159;
+
+// Variables should be in descriptive
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
+
+// Values and Variables
+
+let country = "Japan";
+console.log(country);
+
+let continent = "Asia";
+console.log(continent);
+
+let population = 12000000;
+console.log(population);
+*/
+
+/*
+// Data types
+
+// Number
+let age = 23;
+
+// String
+let name = "Dom";
+
+// Boolean
+let fullAge = true;
+
+// Undefined
+let children;
+
+// Null
+
+// Symbol
+
+// BigInt
+
+
+let javascriptIsFun = false;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Dom"); //For string quotes is must
+
+javascriptIsFun = "YES!"; //Dynamic Typing
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null); //Type of null is a object
+
+let isIland = false;
+console.log(typeof isIland);
+
+let language
+console.log(typeof language);
+
+let country = "Japan";
+console.log(typeof country);
+
+let population = 12000000;
+console.log(typeof population);
+*/
+
+
+
+/*
+// let, const and var
+let age = 30;
+aga = 31; //Variable can be mutated
+
+const birthYear = 1991;
+// birthYear = 1990; //Variable cannot be mutated
+
+// const job; //not legal to put empty variable in const
+
+var job = "Programmer";
+job = "Teacher";
+
+lastName = "Moxley";
+console.log(lastName);
+*/
+
+
+
+
+
+
