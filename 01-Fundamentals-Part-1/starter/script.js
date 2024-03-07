98627cb449f7ede13bb29c2144203789d22595d6
Arnab-AK <royjason715@gmail.com>
2024-03-07 21:55:11 +0530

    Function Declarations vs. Expressions

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -442,7 +442,44 @@ if (numNeighbours === 1) {
 } else {
     console.log('No border');
 }
+
+// Logical Operators
+if (language === 'English' && population < 50 && !isIland) {
+    console.log(`You should live in ${country}`);
+} else {
+    console.log(`${country} does not meet your criteria`);
+}
+
+// The switch Statement
+const language = 'bengali';
+
+switch(language) {
+    case 'chinese': // day === 'monday'
+    case 'mandarin':
+        console.log('MOST number of native speakers!');
+        break;
+    case 'spanish':
+        console.log('2nd place in number of native speakers');
+        break
+    case 'english':
+        console.log('3rd place');
+        break
+    case 'hindi':
+        console.log('Number 4');
+        break
+    case 'arabic':
+        console.log('5th most spoken language');
+        break
+    default:
+        console.log('Great language too :D');
+}   
+
+// The Conditional (Ternary) Operator
+const population = 10;
+population > 33 ? console.log("Portugal's population is above average") :
+console.log("Portugal's population is below average");
 */
+
 
 // Challenges
 
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -2,6 +2,7 @@
 
 /*
 // Activating Strict Mode
+
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -10,4 +11,52 @@ if (hasDriversLicense) console.log('I can drive :D');
 
 // const interface = 'Audio';
 // const private = 543;
+*/
+
+
+/*
+// Functions ****
+
+function logger() {
+    console.log('My name is Jonas');
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor (appels, oranges) {
+    const juice = `Juice with ${appels} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+*/
+
+
+/*
+// Function Declarations vs. Expressions
+
+// Function Declearation
+function calcAge1(birthYear) {
+    const age = 2037 - birthYear;
+    return age;
+}
+const age1 = calcAge1(2000);
+console.log(age1);
+
+// Function Expression
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+const age2 = calcAge2(1990);
+console.log(age2);
+
+console.log(age1, age2);
 */
