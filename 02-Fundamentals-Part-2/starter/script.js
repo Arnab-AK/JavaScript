c19718a355e55909cc0d9e8da341126ecdc9fafb
Arnab-AK <royjason715@gmail.com>
2024-03-08 21:48:20 +0530

    Functions Calling Other Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -60,3 +60,36 @@ console.log(age2);
 
 console.log(age1, age2);
 */
+
+/*
+// Arrow Functions
+const calcAge3 = birthYear => 2037 - birthYear; // Arrow Function
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirementAge = 65 - age;
+    // return retirementAge;
+    return `${firstName} retires in ${retirementAge} years`
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1980, 'Bob'));
+*/
+
+/*
+// Functions Calling Other Functions
+function cutFruitsPieces (fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor (appels, oranges) {
+    const applePieces = cutFruitsPieces(appels);
+    const oragnePieces = cutFruitsPieces(oranges);
+    const juice = `Juice with ${applePieces} piece of apples and ${oragnePieces} piece of oranges.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2, 3));
+*/
