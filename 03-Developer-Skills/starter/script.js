6becaf1b56f00a27e13156834d98fcb02120b31e
Arnab-AK <royjason715@gmail.com>
2024-03-26 21:56:04 +0530

    Setting up Prettier and VS Code || Installing Node.js and Setting Up a Dev Environment

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,11 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+/*
+// Setting up Prettier and VS Code || Installing Node.js and Setting Up a Dev Environment
+const x = '23';
+if (x === 23) console.log(23);
+
+const caclAge = birthYear => 2037 - birthYear;
+console.log(caclAge(1991));
+*/
