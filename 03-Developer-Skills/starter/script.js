0fae2c1abdfb4d3cf4ea642927c1dcf80eda38bd
Arnab-AK <royjason715@gmail.com>
2024-04-10 22:12:56 +0530

    Debugging (Fixing Errors)

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -11,3 +11,47 @@ console.log(caclAge(1991));
 */
 
 // Learning How to Code || How to Think Like a Developer: Become a Problem Solver!
+
+/*
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (temps[i] > max) max = curTemp;
+    if (temps[i] < min) min = curTemp;
+  }
+
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitude = calcTempAmplitude(temperatures);
+console.log(amplitude);
+
+const calcTempAmplitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (temps[i] > max) max = curTemp;
+    if (temps[i] < min) min = curTemp;
+  }
+
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitude = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
+console.log(amplitude);
+*/
